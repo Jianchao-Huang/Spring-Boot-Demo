1b7d264775bcfbe6154b25e9ffcf9937a8606977
Jianchao Huang <jh6940@nyu.edu>
2021-01-19 03:29:55 -0500

    added swagger

---
+++ src/main/java/jackie/Contact.java
@@ -0,0 +1,31 @@
+package jackie;
+
+public class Contact {
+    private String id;
+    private String name;
+    private String phone;
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
